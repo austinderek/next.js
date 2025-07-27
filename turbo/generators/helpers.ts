a3cd18229b491b5332c0b8aa2e35a9852db364cd
keitosuwahara <keitosuwahara0816@gmail.com>
2025-07-27 17:28:40 +0900

    Removed potential bug in toFileName function

--- turbo/generators/helpers.ts
+++ turbo/generators/helpers.ts
@@ -1,7 +1,13 @@
 import type { NodePlopAPI } from 'node-plop'
 
 export function toFileName(str: string) {
-  return str.toLowerCase().replace(/ /g, '-')
+  return str
+    .toLowerCase()
+    .replace(/\s+/g, '-') // Replace one or more spaces with hyphens
+    .replace(/[^a-z0-9-]/g, '') // Remove all but alphanumeric characters and hyphens
+    .replace(/--+/g, '-') // Combine consecutive hyphens into one
+    .replace(/^-+/, '') // Remove leading hyphen
+    .replace(/-+$/, '') // remove trailing hyphen
 }
 
 export function init(plop: NodePlopAPI): void {
