1b88c3a66a779e1609950246e1faff5ddbdd07b5
Jiachi Liu <inbox@huozhi.im>
2025-07-30 17:12:41 +0200

    update comment to trigger deployment

--- test/e2e/og-api/index.test.ts
+++ test/e2e/og-api/index.test.ts
@@ -4,6 +4,8 @@ import { fetchViaHTTP, renderViaHTTP } from 'next-test-utils'
 import fs from 'fs-extra'
 import { join } from 'path'
 
+// Update comment to trigger deployment test
+
 describe('og-api', () => {
   let next: NextInstance
 
