d1ca365a095305475c19fca5afdce7aece099cf0
Trevor Burnham <trevorburnham@gmail.com>
2025-07-29 23:39:10 -0400

    feat(eslint-plugin): expose flatConfigs as property of default export
    Currently, the flat configs object is only available as a named export
    named `flatConfig`, which means that the usage example on #73873 (where
    it's used as a property of the default export named `flatConfigs`)
    doesn't work.
    
    If you try to use `flatConfig` as a property of the default export, it
    works but causes TypeScript errors if the consumer has type checking
    enabled in their ESLint config:
    
    ```
    eslint.config.mjs:180:24 - error TS2339: Property 'flatConfig' does not exist on type '{ rules: { 'google-font-display': RuleModule; 'google-font-preconnect': RuleModule; 'inline-script-id': RuleModule; 'next-script-for-ga': RuleModule; ... 16 more ...; 'no-unwanted-polyfillio': RuleModule; }; configs: { ...; }; }'.
    
    180       eslintPluginNext.flatConfig.recommended,
                               ~~~~~~~~~~
    ```
    
    This has caused quite a bit of confusion, as evidenced by activity on
    https://github.com/vercel/next.js/discussions/49337 from after that PR
    was merged. (I shared that confusion for a while!)
    
    This PR attaches `flatConfig` to the default export as a property named
    `flatConfigs`, while continuing to make it available as a named export
    named `flatConfig` for backward compatibility. With this change, the
    flat config is more easily usable like those of other popular ESLint
    libraries:
    
    ```
    // eslint.config.mjs
    import js from "@eslint/js";
    import reactPlugin from 'eslint-plugin-react';
    import eslintPluginNext from '@next/eslint-plugin-next';
    
    export default [
      ...js.configs.recommended,
      ...reactPlugin.configs.flat.recommended,
      ...eslintPluginImportX.flatConfigs.recommended,
    ]
    ```
    
    I've also reorganized the code a bit, and added proper type checking
    through `@eslint/types`.

--- packages/eslint-plugin-next/src/index.ts
+++ packages/eslint-plugin-next/src/index.ts
@@ -1,4 +1,50 @@
-const recommendedRules = {
+import type { ESLint, Linter, Rule } from 'eslint'
+
+const rules: Record<string, Rule.RuleModule> = {
+  'google-font-display':
+    require('./rules/google-font-display') as typeof import('./rules/google-font-display'),
+  'google-font-preconnect':
+    require('./rules/google-font-preconnect') as typeof import('./rules/google-font-preconnect'),
+  'inline-script-id':
+    require('./rules/inline-script-id') as typeof import('./rules/inline-script-id'),
+  'next-script-for-ga':
+    require('./rules/next-script-for-ga') as typeof import('./rules/next-script-for-ga'),
+  'no-assign-module-variable':
+    require('./rules/no-assign-module-variable') as typeof import('./rules/no-assign-module-variable'),
+  'no-async-client-component':
+    require('./rules/no-async-client-component') as typeof import('./rules/no-async-client-component'),
+  'no-before-interactive-script-outside-document':
+    require('./rules/no-before-interactive-script-outside-document') as typeof import('./rules/no-before-interactive-script-outside-document'),
+  'no-css-tags':
+    require('./rules/no-css-tags') as typeof import('./rules/no-css-tags'),
+  'no-document-import-in-page':
+    require('./rules/no-document-import-in-page') as typeof import('./rules/no-document-import-in-page'),
+  'no-duplicate-head':
+    require('./rules/no-duplicate-head') as typeof import('./rules/no-duplicate-head'),
+  'no-head-element':
+    require('./rules/no-head-element') as typeof import('./rules/no-head-element'),
+  'no-head-import-in-document':
+    require('./rules/no-head-import-in-document') as typeof import('./rules/no-head-import-in-document'),
+  'no-html-link-for-pages':
+    require('./rules/no-html-link-for-pages') as typeof import('./rules/no-html-link-for-pages'),
+  'no-img-element':
+    require('./rules/no-img-element') as typeof import('./rules/no-img-element'),
+  'no-page-custom-font':
+    require('./rules/no-page-custom-font') as typeof import('./rules/no-page-custom-font'),
+  'no-script-component-in-head':
+    require('./rules/no-script-component-in-head') as typeof import('./rules/no-script-component-in-head'),
+  'no-styled-jsx-in-document':
+    require('./rules/no-styled-jsx-in-document') as typeof import('./rules/no-styled-jsx-in-document'),
+  'no-sync-scripts':
+    require('./rules/no-sync-scripts') as typeof import('./rules/no-sync-scripts'),
+  'no-title-in-document-head':
+    require('./rules/no-title-in-document-head') as typeof import('./rules/no-title-in-document-head'),
+  'no-typos': require('./rules/no-typos') as typeof import('./rules/no-typos'),
+  'no-unwanted-polyfillio':
+    require('./rules/no-unwanted-polyfillio') as typeof import('./rules/no-unwanted-polyfillio'),
+}
+
+const recommendedRules: Linter.RulesRecord = {
   // warnings
   '@next/next/google-font-display': 'warn',
   '@next/next/google-font-preconnect': 'warn',
@@ -24,70 +70,29 @@ const recommendedRules = {
   '@next/next/no-script-component-in-head': 'error',
 }
 
-const coreWebVitalsRules = {
+const coreWebVitalsRules: Linter.RulesRecord = {
   '@next/next/no-html-link-for-pages': 'error',
   '@next/next/no-sync-scripts': 'error',
 }
 
-const plugin = {
-  rules: {
-    'google-font-display':
-      require('./rules/google-font-display') as typeof import('./rules/google-font-display'),
-    'google-font-preconnect':
-      require('./rules/google-font-preconnect') as typeof import('./rules/google-font-preconnect'),
-    'inline-script-id':
-      require('./rules/inline-script-id') as typeof import('./rules/inline-script-id'),
-    'next-script-for-ga':
-      require('./rules/next-script-for-ga') as typeof import('./rules/next-script-for-ga'),
-    'no-assign-module-variable':
-      require('./rules/no-assign-module-variable') as typeof import('./rules/no-assign-module-variable'),
-    'no-async-client-component':
-      require('./rules/no-async-client-component') as typeof import('./rules/no-async-client-component'),
-    'no-before-interactive-script-outside-document':
-      require('./rules/no-before-interactive-script-outside-document') as typeof import('./rules/no-before-interactive-script-outside-document'),
-    'no-css-tags':
-      require('./rules/no-css-tags') as typeof import('./rules/no-css-tags'),
-    'no-document-import-in-page':
-      require('./rules/no-document-import-in-page') as typeof import('./rules/no-document-import-in-page'),
-    'no-duplicate-head':
-      require('./rules/no-duplicate-head') as typeof import('./rules/no-duplicate-head'),
-    'no-head-element':
-      require('./rules/no-head-element') as typeof import('./rules/no-head-element'),
-    'no-head-import-in-document':
-      require('./rules/no-head-import-in-document') as typeof import('./rules/no-head-import-in-document'),
-    'no-html-link-for-pages':
-      require('./rules/no-html-link-for-pages') as typeof import('./rules/no-html-link-for-pages'),
-    'no-img-element':
-      require('./rules/no-img-element') as typeof import('./rules/no-img-element'),
-    'no-page-custom-font':
-      require('./rules/no-page-custom-font') as typeof import('./rules/no-page-custom-font'),
-    'no-script-component-in-head':
-      require('./rules/no-script-component-in-head') as typeof import('./rules/no-script-component-in-head'),
-    'no-styled-jsx-in-document':
-      require('./rules/no-styled-jsx-in-document') as typeof import('./rules/no-styled-jsx-in-document'),
-    'no-sync-scripts':
-      require('./rules/no-sync-scripts') as typeof import('./rules/no-sync-scripts'),
-    'no-title-in-document-head':
-      require('./rules/no-title-in-document-head') as typeof import('./rules/no-title-in-document-head'),
-    'no-typos':
-      require('./rules/no-typos') as typeof import('./rules/no-typos'),
-    'no-unwanted-polyfillio':
-      require('./rules/no-unwanted-polyfillio') as typeof import('./rules/no-unwanted-polyfillio'),
+const configs: Record<string, Linter.LegacyConfig> = {
+  recommended: {
+    plugins: ['@next/next'],
+    rules: recommendedRules,
   },
-  configs: {
-    recommended: {
-      plugins: ['@next/next'],
-      rules: recommendedRules,
-    },
-    'core-web-vitals': {
-      plugins: ['@next/next'],
-      extends: ['plugin:@next/next/recommended'],
-      rules: coreWebVitalsRules,
-    },
+  'core-web-vitals': {
+    plugins: ['@next/next'],
+    extends: ['plugin:@next/next/recommended'],
+    rules: coreWebVitalsRules,
   },
 }
 
-const flatConfig = {
+const plugin: ESLint.Plugin = {
+  rules,
+  configs,
+}
+
+const flatConfig: Record<string, Linter.Config> = {
   recommended: {
     name: 'next/recommended',
     plugins: {
@@ -107,6 +112,5 @@ const flatConfig = {
   },
 }
 
-export default plugin
-export const { rules, configs } = plugin
-export { flatConfig }
+export default { ...plugin, flatConfigs: flatConfig }
+export { configs, flatConfig, rules }
