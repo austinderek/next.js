1d20eeef71f25127aad1db57d690d8cd2f6b8733
devjiwonchoi <devjiwonchoi@gmail.com>
2025-08-05 11:08:37 +0200

    need to group the if case

--- packages/next/src/server/base-server.ts
+++ packages/next/src/server/base-server.ts
@@ -2344,7 +2344,7 @@ export default abstract class Server<
     // On minimal mode, the request url of dynamic route can be a
     // literal dynamic route ('/[slug]') instead of actual URL, so overwriting to initPathname
     // will transform back the resolved url to the dynamic route pathname.
-    if (this.minimalMode && !isErrorPathname) {
+    if (!(this.minimalMode && isErrorPathname)) {
       request.url = `${initPathname}${parsedInitUrl.search || ''}`
     }
 
