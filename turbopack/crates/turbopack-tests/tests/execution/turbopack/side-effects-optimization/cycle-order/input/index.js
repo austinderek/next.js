ac893669d67f2521f93eaa9311bb611d894d320c
Tobias Koppers <tobias.koppers@googlemail.com>
2025-08-07 12:23:47 +0200

    fixup test case

--- turbopack/crates/turbopack-tests/tests/execution/turbopack/side-effects-optimization/cycle-order/input/index.js
+++ turbopack/crates/turbopack-tests/tests/execution/turbopack/side-effects-optimization/cycle-order/input/index.js
@@ -2,5 +2,5 @@ import { something } from './main'
 
 it('correct order', () => {
   expect(something).toBe('inner')
-  expect(globalThis.order).toEqual(['inner', 'module'])
+  expect(globalThis.order).toEqual(['inner throws', 'module', 'main'])
 })
--- turbopack/crates/turbopack-tests/tests/execution/turbopack/side-effects-optimization/cycle-order/input/inner.js
+++ turbopack/crates/turbopack-tests/tests/execution/turbopack/side-effects-optimization/cycle-order/input/inner.js
@@ -1,3 +1,9 @@
 import { value } from './module.js'
 export const something = 'inner'
-;(globalThis.order ??= []).push('inner')
+let throws = false
+try {
+  value
+} catch {
+  throws = true
+}
+;(globalThis.order ??= []).push(`inner ${throws ? 'throws' : 'no-throws'}`)
